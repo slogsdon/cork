1f2a6620eaae7e080cacd8c5daa171f8375174af
Shane Logsdon <shane@shanelogsdon.com>
2018-02-15 15:05:08 -0500

    convert test project to mstest

---
+++ test/Spile.Test/LibraryTests.fs
@@ -0,0 +1,37 @@
+module Spile.Test.LibraryTests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+open Spile
+open Spile.Connection
+
+let assertOkResult result =
+  match result with
+  | Ok _ -> Assert.IsTrue(true)
+  | Error _ -> Assert.Fail()
+
+// new class type extending abstract
+type OkSpile () =
+  inherit AbstractSpile()
+
+// new object expression implementing interface
+let TestSpile () =
+  { new ISpile with
+      member __.Init options = options
+      member __.Call _ conn = Ok conn }
+
+[<TestClass>]
+type TestClass () =
+
+  [<TestMethod>]
+  member __.TestMethodPassing () =
+    let conn = defaultConnection
+
+    let spiles : (ISpile * Options) list = [
+      OkSpile() :> ISpile, defaultSpileOptions
+      TestSpile(), defaultSpileOptions
+    ]
+
+    conn
+    |> run spiles
+    |> assertOkResult
